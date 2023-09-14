11715aa1e81efdff8318baf64c7f5004f4981f6e
kalypa <80308782+kalypa@users.noreply.github.com>
2023-09-14 14:30:43 +0900

    몬스터 스폰 위치 수정

--- Assets/Scripts/Monster/Spawner.cs
+++ Assets/Scripts/Monster/Spawner.cs
@@ -8,23 +8,41 @@ public class Spawner : MonoBehaviour
 {
     public List<GameObject> monsters = new();
     public List<Transform> spawnPos = new();
+
+    bool isFirstSpawn = true;
     void Start()
     {
+        StartSpawn();
         StartCoroutine(SpawnMonster(GameManager.Inst.currentStage));
     }
+
+    void StartSpawn()
+    {
+        for(int i = 0; i < spawnPos.Count; i++)
+        {
+            var monster = PoolManager.Instance.GetFromPool<MonsterEvent>(GameManager.Inst.currentStage);
+            monster.gameObject.transform.position = spawnPos[i].position;
+            GameManager.Inst.monsterCount += 1;
+        }
+        isFirstSpawn = false;
+    }
+
     IEnumerator SpawnMonster(int idx)
     {
         while (true)
         {
-            if (GameManager.Inst.monsterCount < 20)
+            if(!isFirstSpawn)
             {
-                int randomPosIdx = Random.Range(0, spawnPos.Count);
-                var monster = PoolManager.Instance.GetFromPool<MonsterEvent>(idx);
-                monster.gameObject.transform.position = spawnPos[randomPosIdx].position;
-                GameManager.Inst.monsterCount += 1;
-            }
+                if (GameManager.Inst.monsterCount < spawnPos.Count)
+                {
+                    int randomPosIdx = Random.Range(0, spawnPos.Count);
+                    var monster = PoolManager.Instance.GetFromPool<MonsterEvent>(idx);
+                    monster.gameObject.transform.position = spawnPos[randomPosIdx].position;
+                    GameManager.Inst.monsterCount += 1;
+                }
 
-            yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(0.5f);
+            }
         }
     }
 }
