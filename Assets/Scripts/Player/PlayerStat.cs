d9ed24506c9375db73689cb88a96014fc81e81ec
kalypa <80308782+kalypa@users.noreply.github.com>
2023-09-16 01:44:20 +0900

    수정

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -9,12 +9,15 @@ public class PlayerStat : ScriptableObject
     public int atk;
     public int maxHp;
     public int hp;
+    public int hpSpeed;
     public float mp;
     public float maxMp;
     public int mpSpeed;
     public int maxXp;
     public int xp;
     public float atkSpeed;
+    public int healHp;
+    public int healMp;
     public int whitePassiveAtk;
     public int darkPassiveAtk;
     public int normalPassiveAtk;
