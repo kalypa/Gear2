a8eef7ed9e8f9107ac30d96eeb816dc84e113195
kalypa <80308782+kalypa@users.noreply.github.com>
2023-09-16 22:14:58 +0900

    스킬 사운드

--- Assets/Scripts/UI/StatCharacter.cs
+++ Assets/Scripts/UI/StatCharacter.cs
@@ -7,6 +7,11 @@ public class StatCharacter : MonoBehaviour
 {
     public Image characterImage;
     public Sprite[] characters = new Sprite[3];
+    RectTransform rect;
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
     void Update()
     {
         SetStatCharacter();
@@ -14,6 +19,8 @@ public class StatCharacter : MonoBehaviour
 
     void SetStatCharacter()
     {
+        if (GameManager.Inst.playerTransform.playermode != 1) rect.sizeDelta = new Vector2(22 * 6, 108);
+        else rect.sizeDelta = new Vector2(90, 108);
         characterImage.sprite = characters[GameManager.Inst.playerTransform.playermode - 1];
     }
 }
